4e855f896349da4a815c974afed5fe8784062700
daiji <jialu.dai@edu.unifi.it>
2025-04-20 21:22:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+console.log('Happy developing ✨')
+const menuBtn = document.getElementById("menuBtn");
+const userIcon = document.getElementById("userIcon");
+const leftMenu = document.getElementById("leftMenu");
+const rightMenu = document.getElementById("rightMenu");
+const searchBtn = document.getElementById("searchBtn");
+const searchBar = document.getElementById("searchBar");
+const header = document.getElementById("mainHeader");
+const secondRow = document.querySelector(".second-row");
+const thirdRow = document.querySelector(".third-row");
+const mobileTitle = document.querySelector(".boston-herald-mobile");
+const desktopTitle = document.querySelector(".boston-herald-desktop");
+
+
+// Toggle left slide menu
+menuBtn.addEventListener("click", () => {
+    leftMenu.classList.toggle("open");
+});
+// Close left menu when clicking outside
+document.addEventListener("click", (event) => {
+    if (!leftMenu.contains(event.target) && !menuBtn.contains(event.target)) {
+        leftMenu.classList.remove("open");
+    }
+});
+
+// Toggle right slide menu
+userIcon.addEventListener("click", () => {
+    rightMenu.classList.toggle("open");
+});
+// Close right menu when clicking outside
+document.addEventListener("click", (event) => {
+    if (!rightMenu.contains(event.target) && !userIcon.contains(event.target)) {
+        rightMenu.classList.remove("open");
+    }
+});
+
+
+// Toggle search bar
+searchBtn.addEventListener("click", () => {
+    searchBar.classList.toggle("hidden");
+});
+
+//scroll
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+
+    thirdRow.classList.toggle("scrolled", scrollTop > 100);
+    secondRow.classList.toggle("scrolled", scrollTop > 100);
+    mobileTitle.classList.toggle("scrolled", scrollTop > 100);
+});
